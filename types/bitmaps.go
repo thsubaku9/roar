26466e92ec1d034908a0bfe27ed3be3efa64fff7
Kaustubh Joshi <kaustubhkj@gmail.com>
2021-06-23 00:40:49 +0530

    subset superset operations for bitmap

--- types/bitmaps.go
+++ types/bitmaps.go
@@ -22,6 +22,30 @@ func (bmp *Bitmaps) Add(elem uint16) error {
 	return nil
 }
 
+func (bmp *Bitmaps) Remove(elem uint16) error {
+	index := elem / 32
+	offset := elem % 32
+	bmp.Values[index] &= 0xFFFF ^ (1 << offset)
+	return nil
+}
+
+/*
+Clamp(start, stop uint32)
+Clear()
+Copy() Container
+Debug() string
+FlipRange(start, stop uint32)
+Index(element uint32) (uint32, error) //returns the index location of provided element
+IsSuperset(con Container) bool
+Jaccard(con Container) float32
+Max() (uint32, error)
+Min() (uint32, error)
+Pop() (uint32, error)         //removes the element with highest value
+Rank(element uint32) []uint32 //number of elements -le the given number
+Select(index uint32) (uint32, error) //return the element at the i-th index
+SymmetricDifference(con Container) (Container, error)
+*/
+
 func (bmp *Bitmaps) Union(bmp2 *Bitmaps) Bitmaps {
 	_bmp := Bitmaps{
 		Values: make([]uint32, 1024),
@@ -46,15 +70,6 @@ func (bmp *Bitmaps) Intersection(bmp2 *Bitmaps) Bitmaps {
 	return _bmp
 }
 
-func (bmp *Bitmaps) IsDisjoint(bmp2 *Bitmaps) bool {
-	for i := range bmp.Values {
-		if (*bmp).Values[i]&(*bmp2).Values[i] != 0 {
-			return false
-		}
-	}
-	return true
-}
-
 func (bmp *Bitmaps) Difference(bmp2 *Bitmaps) Bitmaps {
 	_bmp := Bitmaps{
 		Values: make([]uint32, 1024),
@@ -64,4 +79,26 @@ func (bmp *Bitmaps) Difference(bmp2 *Bitmaps) Bitmaps {
 		_bmp.Values[i] = (*bmp).Values[i] ^ ((*bmp).Values[i] & (*bmp2).Values[i])
 	}
 	return _bmp
+}
+
+func (bmp *Bitmaps) IsDisjoint(bmp2 *Bitmaps) bool {
+	for i := range bmp.Values {
+		if (*bmp).Values[i]&(*bmp2).Values[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (bmp *Bitmaps) IsSubset(bmp2 *Bitmaps) bool {
+	return bmp2.IsSuperset(bmp)
+}
+
+func (bmp *Bitmaps) IsSuperset(bmp2 *Bitmaps) bool {
+	for i := range bmp.Values {
+		if (*bmp).Values[i]|(*bmp2).Values[i] != (*bmp).Values[i] {
+			return false
+		}
+	}
+	return true
 }
