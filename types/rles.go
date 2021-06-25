ae3c3e7b1cc9ff38c922d85c2071db84ffe5e3ab
Kaustubh Joshi <kaustubhkj@gmail.com>
2021-06-25 19:54:44 +0530

    overlap checking

--- types/rles.go
+++ types/rles.go
@@ -1,6 +1,7 @@
 package roar
 
 import (
+	"fmt"
 	"roar/util"
 )
 
@@ -18,7 +19,7 @@ func CreateRles() Rles {
 }
 
 //findIndex finds the location after which the given elem should be inserted
-/*
+
 func (rle *Rles) findIndex(p rlePair, start, end int) (int, error) {
 	if end < start {
 		return -1, fmt.Errorf("array is empty")
@@ -27,28 +28,29 @@ func (rle *Rles) findIndex(p rlePair, start, end int) (int, error) {
 	mid := (start + end) / 2
 
 	if start == end {
-		if ar.Arr[mid] == elem {
-			return mid, nil
-		}
-		if ar.Arr[mid] < elem {
-			return mid, fmt.Errorf("value doesn't exist")
-		} else {
-			return mid - 1, fmt.Errorf("value doesn't exist")
-		}
+
 	}
-	if ar.Arr[mid] < elem {
-		return ar.findIndex(elem, mid+1, end)
+
+	if rle.RlePairs[mid].Start+rle.RlePairs[mid].RunLen < p.Start {
+		return rle.findIndex(p, mid+1, end)
+	} else if p.Start+p.RunLen < rle.RlePairs[mid].Start {
+		return rle.findIndex(p, start, mid)
 	} else {
-		return ar.findIndex(elem, start, mid)
+		//TODO -> check overlap cases
 	}
 }
-*/
 
-//only does increasing order check
-func (p1 rlePair) checkOverlap(p2 rlePair) bool {
-	return p1.Start+p1.RunLen >= p2.Start
+func (p1 rlePair) lSideOverlap(p2 rlePair) bool {
+	return (p2.Start+p2.RunLen >= p1.Start) && p2.Start+p2.RunLen <= p1.Start+p1.RunLen
 }
 
+func (p1 rlePair) rSideOverlap(p2 rlePair) bool {
+	return (p2.Start <= p1.Start+p1.RunLen) && (p1.Start <= p2.Start)
+}
+
+func (p1 rlePair) isSubSegment(p2 rlePair) bool {
+	return p1.Start >= p2.Start && p1.Start+p1.RunLen <= p2.Start+p2.RunLen
+}
 func (p1 rlePair) mergeReturn(p2 rlePair) rlePair {
 	return rlePair{p1.Start, p1.RunLen + p2.RunLen - (p1.Start + p1.RunLen - p2.Start)}
 }
